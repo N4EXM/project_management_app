bf9fad2c3c57a90fc0db5d91cf24321dabe8f805
N4EXM <naemmmail89@gmail.com>
2024-12-03 14:11:28 +0000

    nothing changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react'
 import HomeIcon from '../assets/HomeIcon.png'
 import HomeActiveIcon from '../assets/HomeActiveIcon.png'
+import CalendarIcon from '../assets//CalendarIcon.png'
+import CalendarActiveIcon from '../assets/CalendarActiveIcon.png'
+import ProjectsIcon from '../assets/ProjectsIcon.png'
+import ProjectsActiveIcon from '../assets/ProjectsActiveIcon.png'
 
 const Navbar = () => {
 
-  const [active, setActive] = useState(false)
+  const [navLinks, setNavLinks] = useState([
+    {Name: 'Home', Icon: HomeIcon, ActiveIcon: HomeActiveIcon},
+    {Name: 'Projects', Icon: ProjectsIcon, ActiveIcon: ProjectsActiveIcon},
+    {Name: 'Calendar', Icon: CalendarIcon, ActiveIcon: CalendarActiveIcon},
+
+  ])
+  
 
   return (
-    <div className='absolute bottom-0 left-0 flex flex-row items-center w-full p-2 bg-secondaryBackground justify-evenly'>
+    <div className='absolute bottom-0 left-0 flex flex-row items-center justify-around w-full p-1 bg-secondaryBackground'>
 
-      <button className='flex flex-col items-center justify-center'>
-        <img src={active ?  HomeActiveIcon : HomeIcon} alt="Home" className='w-5 h-5' />
-      </button>
+      
 
     </div>
   )
