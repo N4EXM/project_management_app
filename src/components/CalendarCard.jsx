d22391c64a17ad343aaf2633cdfd8e1d7382999f
N4EXM <naemmmail89@gmail.com>
2024-12-12 11:40:15 +0000

    calendarCard needs to change

--- src/components/CalendarCard.jsx
+++ src/components/CalendarCard.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import ArrowIcon from '../assets/ArrowIcon.png'
-import Calendar from 'react-calendar'
-
 
 const CalendarCard = () => {
   return (
-    <div className='flex flex-col items-start justify-start w-full h-full gap-2 p-3 px-5 rounded-lg bg-tertiary'>
-
-      
-      <Calendar className='font-bold'/>
-
-    </div>
+    <div>CalendarCard</div>
   )
 }
 
